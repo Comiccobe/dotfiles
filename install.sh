39e45e74648ac5b183e8e35fc2019a265bd60fdf
Jakob Hillerström <zurric@gmail.com>
2021-01-27 12:06:10 +0100

    Add install script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+mkdir ~/.vim
+mkdir ~/.vim/undodir
+mkdir ~/.vim/swapdir
+mkdir ~/.vim/backupdir
+mkdir ~/.nvm
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
