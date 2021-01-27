9652e4adfc50457e76ef0ee9f6e6d117dcab24d2
Jakob Hillerström <zurric@gmail.com>
2021-01-27 11:41:37 +0100

    Remove dependencies and fix theme scripts

--- dark-theme.sh
+++ dark-theme.sh
@@ -1,6 +1,6 @@
 cd ~/.config/kitty
 rm ~/.config/kitty/theme.conf
-ln -s ./kitty-themes/themes/Solarized_Dark.conf ~/.config/kitty/theme.conf
+ln -s ./solarized-dark.conf ~/.config/kitty/theme.conf
 rm ~/.tmux.theme.conf
 ln -s ./.tmux.theme.dark.conf ~/.tmux.theme.conf
 tmux source-file ~/.tmux.conf
--- light-theme.sh
+++ light-theme.sh
@@ -1,6 +1,6 @@
 cd ~/.config/kitty
 rm ~/.config/kitty/theme.conf
-ln -s ./kitty-themes/themes/Solarized_Light.conf ~/.config/kitty/theme.conf
+ln -s ./solarized-light.conf ~/.config/kitty/theme.conf
 rm ~/.tmux.theme.conf
 ln -s ./.tmux.theme.light.conf ~/.tmux.theme.conf
 tmux source-file ~/.tmux.conf
