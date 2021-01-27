e52035c809bf3db51357b6a2b4d2df4323ef0e3d
Jakob Hillerström <jakob@woocode.com>
2021-01-27 09:10:22 +0100

    Add install script and remove .vim

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
